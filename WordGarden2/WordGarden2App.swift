0b4177ef3443acaec742deaeeba84a272ccd3a9a
Christian Manzaraz <magnusmanz@icloud.com>
2025-01-27 21:26:37 -0300

    Initial Commit

---
+++ WordGarden2/WordGarden2App.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden2App.swift
+//  WordGarden2
+//
+//  Created by Christian Manzaraz on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
